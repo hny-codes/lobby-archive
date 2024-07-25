672f286594a76abffe70324d0ab4beafbd05fccf
Henry <hnguyhenry@protonmail.com>
2024-07-24 18:47:20 -0700

    Add Vercel adapter to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,26 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
 import { loadEnv } from 'vite';
-import vercel from "@astrojs/vercel/serverless";
-const {
-  PUBLIC_SANITY_PROJECT_ID,
-  PUBLIC_SANITY_DATASET
-} = loadEnv(process.env.NODE_ENV, process.cwd(), '');
-
+import vercel from '@astrojs/vercel/serverless';
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  ''
+);
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [tailwind(), react(), sanity({
-    projectId: PUBLIC_SANITY_PROJECT_ID,
-    dataset: PUBLIC_SANITY_DATASET,
-    useCdn: false,
-    apiVersion: '2024-07-17',
-    studioBasePath: '/admin' // If you want to access the Studio on a route
-  })],
-  adapter: vercel()
+  integrations: [
+    tailwind(),
+    react(),
+    sanity({
+      projectId: PUBLIC_SANITY_PROJECT_ID,
+      dataset: PUBLIC_SANITY_DATASET,
+      useCdn: false,
+      apiVersion: '2024-07-17',
+      studioBasePath: '/admin', // If you want to access the Studio on a route
+    }),
+  ],
+  adapter: vercel(),
 });
