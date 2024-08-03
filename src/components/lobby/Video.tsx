d672a9c1a5202cd28d35dd5952fdd685d9b4ce18
Henry <hnguyhenry@protonmail.com>
2024-08-02 19:01:02 -0700

    Add comments to adjust for future feature

--- src/components/lobby/Video.tsx
+++ src/components/lobby/Video.tsx
@@ -23,9 +23,9 @@ export default function Video({
 }: StudentInfo) {
   const [video, setVideo] = useState<string>();
 
-  // state for blurred image until video has fully loaded
-  const [loading, setLoading] = useState(true);
-  const [videoLoading, setVideoLoading] = useState(false);
+  // TODO: state for blurred image until video has fully loaded
+  // const [loading, setLoading] = useState(true);
+  // const [videoLoading, setVideoLoading] = useState(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   //  Fetch video and set video loading state to true
@@ -33,7 +33,7 @@ export default function Video({
     const getVideo = async () => {
       const url: VideoUrl = await fetchVideo(name);
       setVideo(url.video);
-      setVideoLoading(true);
+      // setVideoLoading(true);
     };
 
     getVideo();
@@ -41,22 +41,22 @@ export default function Video({
 
   // once video loading state is true, video loading state waits until video data has loaded (readystate >= 3)
   // once loaded, changes loading state of image to true, thus removing it
-  useEffect(() => {
-    const videoData = (vid: HTMLVideoElement) => {
-      if (vid.readyState >= 3) setLoading(false);
-    };
+  // useEffect(() => {
+  //   const videoData = (vid: HTMLVideoElement) => {
+  //     if (vid.readyState >= 3) setLoading(false);
+  //   };
 
-    if (videoRef.current) {
-      videoRef.current.addEventListener('loadeddata', () =>
-        videoData(videoRef.current as HTMLVideoElement)
-      );
+  //   if (videoRef.current) {
+  //     videoRef.current.addEventListener('loadeddata', () =>
+  //       videoData(videoRef.current as HTMLVideoElement)
+  //     );
 
-      return () =>
-        videoRef.current?.removeEventListener('loadeddata', () =>
-          videoData(videoRef.current as HTMLVideoElement)
-        );
-    }
-  }, [videoLoading]);
+  //     return () =>
+  //       videoRef.current?.removeEventListener('loadeddata', () =>
+  //         videoData(videoRef.current as HTMLVideoElement)
+  //       );
+  //   }
+  // }, [videoLoading]);
 
   return (
     <div className='aspect-video relative'>
